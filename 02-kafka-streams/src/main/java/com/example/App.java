42e393b9001d83ebc9865d3eb84b773ae11e0aff
Mitch Seymour <mitchseymour@gmail.com>
2022-07-10 22:02:46 -0400

    Add Kafka Streams example

---
+++ 02-kafka-streams/src/main/java/com/example/App.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.util.Properties;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.Topology;
+
+public class App {
+  public static void main(String[] args) {
+    // instantiate the topology
+    Topology topology = MyTopology.build();
+
+    // load the Kafka Streams configuration
+    Properties config = AppConfig.getKafkaStreamsConfig();
+
+    // build the topology and start streaming!
+    KafkaStreams streams = new KafkaStreams(topology, config);
+
+    // close the Kafka Streams threads on shutdown
+    Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+    System.out.println("Starting Kafka Streams application");
+    streams.start();
+  }
+}
