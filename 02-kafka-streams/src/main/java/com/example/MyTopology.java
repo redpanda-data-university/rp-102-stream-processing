c7828937ee5e71e4d16482449007b00d942946b7
Mitch Seymour <mitchseymour@gmail.com>
2022-07-10 22:45:56 -0400

    Use count aggregation

--- 02-kafka-streams/src/main/java/com/example/MyTopology.java
+++ 02-kafka-streams/src/main/java/com/example/MyTopology.java
@@ -1,7 +1,10 @@
 package com.example;
 
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Printed;
 
@@ -10,8 +13,15 @@ class MyTopology {
   public static Topology build() {
     StreamsBuilder builder = new StreamsBuilder();
 
-    KStream<byte[], byte[]> stream = builder.stream("purchases");
-    stream.print(Printed.<byte[], byte[]>toSysOut().withLabel("purchases"));
+    builder.stream("purchases", Consumed.with(Serdes.String(), Serdes.String()))
+        // group by the key
+        .groupByKey(Grouped.with(Serdes.String(), Serdes.String()))
+        // count per key
+        .count()
+        // convert the table to a stream
+        .toStream()
+        // print the output
+        .print(Printed.<String, Long>toSysOut().withLabel("purchases"));
 
     return builder.build();
   }
